66194d2d4665a613269d54b0bad0dddb5c876b83
Himanshu Arodia <himanshuarodia5@gmail.com>
2025-01-24 17:21:51 +0000

    Fix import paths in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import Page1 from './pages/page1';
+import React from 'react';
+import Page1 from './pages/Page1';
 import Header from './components/Header';
-import Page2 from './pages/page2'
+import Page2 from './pages/Page2';
 import Page3 from './pages/Page3';
-import Page4 from './pages/page4';
+import Page4 from './pages/Page4';
 import Page5 from './pages/Page5';
 import Page6 from './pages/Page6';
-// import Page7 from './pages/Page7';
+// import Page7 from './pages/Page7'; // Uncomment if needed
+
 
 function App() {
   return (
